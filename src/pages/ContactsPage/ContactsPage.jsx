e9dc1d5db9328ade0a9f72a5c8989a559478d7ee
andriiKrakovskyi <andrii.krakovskyi@gmail.com>
2025-03-07 15:51:42 +0100

    Fix folder case issue try1

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,6 +1,6 @@
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { fetchContacts } from '../../redux/Contacts/operations';
+import { fetchContacts } from '../../redux/contacts/operations';
 
 import ContactForm from '../../components/ContactForm/ContactForm';
 import SearchBox from '../../components/SearchBox/SearchBox';
