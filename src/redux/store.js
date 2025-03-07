f377e0ddbcfe0d14531e7df69f5dd80dcd53a55c
andriiKrakovskyi <andrii.krakovskyi@gmail.com>
2025-03-07 15:11:24 +0100

    test2

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
-import { contactReducer } from './contacts/slice.js';
-import { filterReducer } from './filters/slice';
-import { authReducer } from './auth/slice';
+// import { contactReducer } from './contacts/slice.js';
+// import { filterReducer } from './filters/slice';
+// import { authReducer } from './auth/slice';
+import { authReducer } from './auth/slice.js';
+import { contactReducer } from './test_name/slice.js';
+import { filterReducer } from './filters/slice.js';
 
 import {
   persistStore,
